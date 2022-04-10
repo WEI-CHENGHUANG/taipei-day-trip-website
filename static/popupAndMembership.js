cbb92ca46e0094cff33e3e6db04300126b5a0eb8
Wei-Cheng Huang <z5280627@student.unsw.edu.au>
2022-04-10 21:48:23 +1000

    first

--- static/popupAndMembership.js
+++ static/popupAndMembership.js
@@ -13,13 +13,13 @@ let clearPasswordBoxInputForSignIN =
 let clearPasswordBoxInputForRegister =
   document.getElementsByClassName("passwordBox")[1];
 
-let urlUser = "http://192.168.0.226:3000/api/user";
-// let urlUser = "http://52.63.14.114:3000/api/user";
-let urlBooking = "http://192.168.0.226:3000/api/booking";
-// let urlBooking = "http://52.63.14.114:3000/api/booking";
+// let urlUser = "http://192.168.0.226:3000/api/user";
+// let urlBooking = "http://192.168.0.226:3000/api/booking";
+let urlUser = "http://52.63.14.114:3000/api/user";
+let urlBooking = "http://52.63.14.114:3000/api/booking";
 // This is for checking whether current page's URL is the same as booking page URL and then I can get the contact name.
-let urlCurrentBooking = "http://192.168.0.226:3000/booking"
-// let urlCurrentBooking = "http://52.63.14.114:3000/booking"
+// let urlCurrentBooking = "http://192.168.0.226:3000/booking"
+let urlCurrentBooking = "http://52.63.14.114:3000/booking"
 
 // in index.html This function is the all the log-in function source, which means if I want to use log-in box, I can just use this.
 function btnPushItems_2() {
